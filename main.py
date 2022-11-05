394388a54f58af9880ff11b4cc33c0accecb25ea
Deissson <deisssn@proton.me>
2022-11-05 20:14:15 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -3,71 +3,98 @@ import time
 import datetime as datetime
 import webbrowser as brw
 
-
-def choose():
-    options = ["Flip a Coin", "Search"]
-    availArgs=[]
-    for i, option in enumerate(options):
-        print(f'{i + 1}: {option} ')
-        availArgs.append(i+1)
-    choice=input("Choose an option >>  ")
-    if int(choice) not in availArgs:
-        print(f'No such option "{choice}".')
-    else:
+class tools:
+    def choose(): # Generates a list with options and return a user's choice
+        
+        #List of options for a list
+        options = ["Flip a Coin", "Search"]
+        # Generating a list with options
+        for i, option in enumerate(options):
+            print(f'{i + 1}: {option} ')
+        choice=input("Choose an option >>  ")
         return int(choice)
+            
+
+    def tossCoin(): # Returns Heads/Tails
         
-
-def tossCoin():
-    choice = ["Heads", "Tails"]
-    return choice[random.randint(0,1)]
-
-def multiToss(arg1):
-    count = 0
-    outcome = []
-    while count < arg1:
-        toss = tossCoin()
-        print("Flipping...")
-        time.sleep(1)
-        print(toss)
-        count += 1
-        outcome.append(toss)
-    print(f"----------------")
-    print(f'Heads has occured: {outcome.count("Heads")}\n\nTails has occured: {outcome.count("Tails")}\n')
-
-def google():
-    i=input("Enter your query >>  ")
-
-    if i.startswith(("!g", "!google")):
-        iG = i.replace('!google', '', 1).replace('!g', '', 1)
-        brw.open_new_tab(f'https://www.google.com/search?hl=en&q={iG}')
-
-    elif i.startswith(("!ya", "!y", "!yandex")):
-        iY = i.replace("!yandex", '', 1).replace("!ya", '', 1).replace("!y", '', 1)
-        brw.open_new_tab(f"https://yandex.com/search/?text={iY}")
-
-    else:
-        iD = i.replace('?', '%3F&')
-        brw.open_new_tab(f"https://duckduckgo.com/?q={iD}")
-        
-if __name__ == "__main__":
-
-    def tossit():  
+        #Available options
+        choice = ["Heads", "Tails"]
+        return choice[random.randint(0,1)]
+    
+    def tossOnce(): # Used for tossing a coin once
         i = int(input("How times toss a coin? >>  "))
         if i == 1:
             print("Flipping...")
             time.sleep(1)
-            print(tossCoin())
+            print(tools.tossCoin())
         elif i < 1:
             print(f'Exitting because the given value was "{i}"...')
             exit
         else:
-            multiToss(i)
+            tools.multiToss(i)
+        # TODO: combine tossCoin and tossOnce
+
+    def multiToss(arg1): # Tosses a coin multiple times, heavily based on tossCoin
+        count = 0
+        outcome = []
+        
+        while count < arg1:
+            toss = tools.tossCoin()
+            print("Flipping...")
+            time.sleep(1)
+            print(toss)
+            count += 1
+            outcome.append(toss)
+        
+        print(f"----------------")
+        print(f'Heads has occured: {outcome.count("Heads")}\n\nTails has occured: {outcome.count("Tails")}\n')
+
+    def search(): # Searches the web using 3 search engines
+        i=input("Enter your query >>  ")
+
+        # Search using Google
+        if i.startswith(("!g", "!google")):
+            iG = i.replace('!google', '', 1).replace('!g', '', 1)
+            brw.open_new_tab(f'https://www.google.com/search?hl=en&q={iG}')
+        # Search using Yandex
+        elif i.startswith(("!ya", "!y", "!yandex")):
+            iY = i.replace("!yandex", '', 1).replace("!ya", '', 1).replace("!y", '', 1)
+            brw.open_new_tab(f"https://yandex.com/search/?text={iY}")
+        # Search using DuckDuckGo (Default)
+        else:
+            iD = i.replace('?', '%3F&')
+            brw.open_new_tab(f"https://duckduckgo.com/?q={iD}")
+            
+    def __init__(self) -> None: 
+        choice = tools.choose()
+        if choice == 1:
+            tools.tossOnce()
+        elif choice == 2:
+            tools.search()
+        else:
+            print(f'No such option "{choice}".')
+            exit
+            
+if __name__ == "__main__":
+    # def tossit():  
+    #     i = int(input("How times toss a coin? >>  "))
+    #     if i == 1:
+    #         print("Flipping...")
+    #         time.sleep(1)
+    #         print(tools.tossCoin())
+    #     elif i < 1:
+    #         print(f'Exitting because the given value was "{i}"...')
+    #         exit
+    #     else:
+    #         tools.multiToss(i)
             
             
-    choice = choose()
-    if choice == 1:
-        tossit()
-    elif choice == 2:
-        google()
-    else:
-        exit
+    # choice = tools.choose()
+    # if choice == 1:
+    #     tossit()
+    # elif choice == 2:
+    #     tools.google()
+    # else:
+    #     exit
+    
+    tools()
